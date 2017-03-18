3eb7619ff9e0e635f8ce9184dd5028b4cdaa6a79
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-03-18 11:18:30 +0100

    Add default spring profile.

---
+++ src/main/java/ApplicationWebXml.java
@@ -0,0 +1,23 @@
+import config.DefaultProfileUtil;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+/**
+ * @author Helmut Steiner
+ * @version 1.0
+ * @see org.springframework.boot.web.support.SpringBootServletInitializer
+ * <p>Set default application profile.</p>
+ */
+public class ApplicationWebXml extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+    /**
+     * <p>set default profile when no profile is set</p>
+     *
+     */
+    DefaultProfileUtil.addDefaultProfile(application.application());
+    return application.sources(FarmhandMain.class);
+  }
+}
