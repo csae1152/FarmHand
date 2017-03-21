6444a24939c82ee85691a546e22ff27acd686050
Helmut Steiner <csae1152@uibk.ac.at>
2017-03-21 11:44:50 +0100

    Update FarmhandMain.java

--- src/main/java/FarmhandMain.java
+++ src/main/java/FarmhandMain.java
@@ -43,7 +43,7 @@ public class FarmhandMain {
     Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
     if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
       log.error("You have misconfigured your application! It should not run " +
-        "with both the 'dev' and 'prod' profiles at the same time.");
+        "with both the 'development' and 'production' profiles at the same time.");
     }
     if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
       log.error("You have misconfigured your application! It should not" +
